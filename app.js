aeefed612e051648f5409196c1ece3326dc3467e
Guilherme Mesquita <guirozmesquita@gmail.com>
2022-05-20 11:51:16 -0400

    configurando o Html, Css e o Js

---
+++ app.js
@@ -0,0 +1,12 @@
+let newTodo = document.getElementById('new-todo')
+let todoNew = document.getElementById('todonew')
+
+function colorChange(){
+    newTodo.style.background = "#151515";
+    todoNew.style.background = "#151515";
+}
+
+function colorBack(){
+    newTodo.style.background = "#1b1a1b";
+    todoNew.style.background = "#1b1a1b";
+}
