4ad4d80dc7cec6802f511851b1a3eca1708c7b4b
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-05-21 09:06:16 -0400

    ajustanto o script

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let newTodo = document.getElementById('new-todo')
 let todoNew = document.getElementById('todonew')
 
+let data = [
+    {'tarefa': 'Estudar'}
+]
+
 function colorChange(){
     newTodo.style.background = "#151515";
     todoNew.style.background = "#151515";
@@ -9,4 +13,14 @@ function colorChange(){
 function colorBack(){
     newTodo.style.background = "#1b1a1b";
     todoNew.style.background = "#1b1a1b";
+}
+
+const createItem = (tarefa, status='') => {
+    const item = document.createElement('label');
+    item.classList.add("todo-item");
+    item.innerHTML = `<input type="checkbox" ${status}>
+    <div>${tarefa}</div>
+    <input type="button" value="X">`
+
+    document.getElementById('todoList').appendChild(item)
 }
